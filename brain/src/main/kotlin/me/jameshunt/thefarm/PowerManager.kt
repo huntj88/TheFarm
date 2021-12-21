8a87cc86177d214a7358fdd39d9b2cbba7b92215
huntj88 <huntj88@gmail.com>
2021-12-21 14:44:25 -0600

    getting ready for smart power strip

--- brain/src/main/kotlin/me/jameshunt/thefarm/PowerManager.kt
+++ brain/src/main/kotlin/me/jameshunt/thefarm/PowerManager.kt
@@ -3,13 +3,28 @@ package me.jameshunt.thefarm
 import me.jameshunt.brain.sql.LogQueries
 
 class PowerManager(private val logQueries: LogQueries) {
-    enum class PlugAlias {
-        Lights,
-        Water
+    private val powerStripIp: String = "0.0.0.0" // TODO
+
+    enum class PlugAlias(val id: String) {
+        Lights("TODO Lights"),
+        Water("TODO Water")
     }
 
     fun setState(plugAlias: PlugAlias, on: Boolean) {
-        logQueries.insert("POWER", "$plugAlias, on: $on")
-        // TODO
+        logQueries.insert("POWER", "$plugAlias, setting state: $on")
+//        setStateCommand(plugAlias, on)
+    }
+
+    private fun setStateCommand(plugAlias: PlugAlias, on: Boolean) {
+        val state = if (on) 1 else 0
+        val setChildPlugStateJson = """
+            {
+                "context":{"child_ids":["${plugAlias.id}"]}, 
+                "system":{"set_relay_state":{"state":$state}}
+            }
+            """.trimIndent()
+
+        "./tplink_smartplug.py -t $powerStripIp -j $setChildPlugStateJson".exec()
+            //.also { logQueries.insert("POWER", "Response: $it") }
     }
 }
