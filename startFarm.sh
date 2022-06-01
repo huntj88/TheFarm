43c7f0839eda99ea4799b399e948997ecbedae92
huntj88 <huntj88@gmail.com>
2022-05-31 22:37:11 -0500

    SIGTERM passed to java process properly

--- startFarm.sh
+++ startFarm.sh
@@ -4,4 +4,15 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1 ; pwd -P )
 cd "$parent_path" || exit 1
 
-java -jar farmBrain-1.0-classifier.jar > $(date +"farm-%Y-%m-%dT%H:%M:%S%:z.log") 2>&1
+stopFarm() {
+  echo "Caught SIGTERM signal!"
+  kill -TERM "$child" 2>/dev/null
+}
+# make sure SIGTERM sent to java process
+trap stopFarm SIGTERM
+
+# actual command to start java process and send logs to dated file
+java -jar farmBrain-1.0-classifier.jar > $(date +"farm-%Y-%m-%dT%H:%M:%S%:z.log") 2>&1 &
+
+child=$!
+wait "$child"
