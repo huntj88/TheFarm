94b8618aa4aea39a4c745b7494d767246941631f
huntj88 <huntj88@gmail.com>
2022-12-28 01:43:39 -0600

    better timing and cleanup

--- arduino/IrrigationRotation/IrrigationRotation/IrrigationRotation.ino
+++ arduino/IrrigationRotation/IrrigationRotation/IrrigationRotation.ino
@@ -5,14 +5,12 @@ int minPulseWidth = 500;
 int maxPulseWidth = 2500;
 int numSecondsToWater = 3;
 int numSecondsToReturn = 6;
+int waterPin = 0; // D3
 
 void setup() {
-  pinMode(0, OUTPUT); // D3
-  
-
-  // turn on water, delay to allow time for water to flow
-  digitalWrite(0, HIGH);
-  // delay(500);
+  // setup and turn on water
+  pinMode(waterPin, OUTPUT);
+  digitalWrite(waterPin, HIGH);
 
   servo.attach(2, minPulseWidth, maxPulseWidth); // D4
 }
@@ -24,32 +22,36 @@ void loop() {
   int delayMilliPerDegreeWater = numSecondsToWater * 1000 / 360.0;
 
   // SLOW START ROTATION START
-  for (float angle = 0; angle < 12; angle += degreeToIncrement) {
+  for (float angle = 0; angle < 8; angle += degreeToIncrement) {
     servo.writeMicroseconds((angle / 360.0 * pulseWidthDiff) + minPulseWidth);
     delay(delayMilliPerDegreeWater * degreeToIncrement * 4);
   }
 
-  for (float angle = 12; angle < 24; angle += degreeToIncrement) {
+  for (float angle = 8; angle < 16; angle += degreeToIncrement) {
     servo.writeMicroseconds((angle / 360.0 * pulseWidthDiff) + minPulseWidth);
     delay(delayMilliPerDegreeWater * degreeToIncrement * 3);
   }
 
-  for (float angle = 24; angle < 36; angle += degreeToIncrement) {
+  for (float angle = 16; angle < 24; angle += degreeToIncrement) {
     servo.writeMicroseconds((angle / 360.0 * pulseWidthDiff) + minPulseWidth);
     delay(delayMilliPerDegreeWater * degreeToIncrement * 2);
   }
   // SLOW START ROTATION END
 
   // angle starts from end of slow rotation
-  for (float angle = 36; angle < 360; angle += degreeToIncrement) {
+  for (float angle = 24; angle < 360; angle += degreeToIncrement) {
     servo.writeMicroseconds((angle / 360.0 * pulseWidthDiff) + minPulseWidth);
+
+    // turn water pressure off 1 second before end of irrigation period
+    int turnOffWaterDegrees = 360 - (1000 / delayMilliPerDegreeWater);
+    if (angle >= turnOffWaterDegreess) {
+      digitalWrite(0, LOW);
+    }
+
     delay(delayMilliPerDegreeWater * degreeToIncrement);
   }
   
   servo.writeMicroseconds(maxPulseWidth);
-  
-  // turn water off
-  digitalWrite(0, LOW);
   
   delay(1000);
 
