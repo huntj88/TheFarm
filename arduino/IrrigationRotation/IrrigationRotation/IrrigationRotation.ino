05dafbd9930e82717603c200413164ff26fdb605
huntj88 <huntj88@gmail.com>
2022-12-28 01:05:14 -0600

    added relay to code

--- arduino/IrrigationRotation/IrrigationRotation/IrrigationRotation.ino
+++ arduino/IrrigationRotation/IrrigationRotation/IrrigationRotation.ino
@@ -3,45 +3,55 @@
 Servo servo;
 int minPulseWidth = 500;
 int maxPulseWidth = 2500;
-int numSecondsToWater = 5;
-int numSecondsToReturn = 15;
+int numSecondsToWater = 3;
+int numSecondsToReturn = 6;
 
 void setup() {
-  servo.attach(2, minPulseWidth, maxPulseWidth); //D4
+  pinMode(0, OUTPUT); // D3
+  
+
+  // turn on water, delay to allow time for water to flow
+  digitalWrite(0, HIGH);
+  // delay(500);
+
+  servo.attach(2, minPulseWidth, maxPulseWidth); // D4
 }
 
 void loop() {
   int pulseWidthDiff = maxPulseWidth - minPulseWidth;
 
   int degreeToIncrement = 2;
-  int delayMilliPerDegreeWater = numSecondsToWater * 1000 / 360.0 / degreeToIncrement;
+  int delayMilliPerDegreeWater = numSecondsToWater * 1000 / 360.0;
 
   // SLOW START ROTATION START
-  for (float angle = 0; angle < 16; angle += degreeToIncrement) {
+  for (float angle = 0; angle < 12; angle += degreeToIncrement) {
     servo.writeMicroseconds((angle / 360.0 * pulseWidthDiff) + minPulseWidth);
-    delay(delayMilliPerDegreeWater * 6);
+    delay(delayMilliPerDegreeWater * degreeToIncrement * 4);
   }
 
-  for (float angle = 16; angle < 32; angle += degreeToIncrement) {
+  for (float angle = 12; angle < 24; angle += degreeToIncrement) {
     servo.writeMicroseconds((angle / 360.0 * pulseWidthDiff) + minPulseWidth);
-    delay(delayMilliPerDegreeWater * 4);
+    delay(delayMilliPerDegreeWater * degreeToIncrement * 3);
   }
 
-  for (float angle = 32; angle < 64; angle += degreeToIncrement) {
+  for (float angle = 24; angle < 36; angle += degreeToIncrement) {
     servo.writeMicroseconds((angle / 360.0 * pulseWidthDiff) + minPulseWidth);
-    delay(delayMilliPerDegreeWater * 2);
+    delay(delayMilliPerDegreeWater * degreeToIncrement * 2);
   }
   // SLOW START ROTATION END
 
   // angle starts from end of slow rotation
-  for (float angle = 64; angle < 360; angle += degreeToIncrement) {
+  for (float angle = 36; angle < 360; angle += degreeToIncrement) {
     servo.writeMicroseconds((angle / 360.0 * pulseWidthDiff) + minPulseWidth);
-    delay(delayMilliPerDegreeWater);
+    delay(delayMilliPerDegreeWater * degreeToIncrement);
   }
-
+  
   servo.writeMicroseconds(maxPulseWidth);
-  delay(2000);
-
+  
+  // turn water off
+  digitalWrite(0, LOW);
+  
+  delay(1000);
 
   int delayMilliPerDegreeReturn = numSecondsToReturn * 1000 / 360.0;
   for (float angle = 360; angle > 0; angle -= 1) {
